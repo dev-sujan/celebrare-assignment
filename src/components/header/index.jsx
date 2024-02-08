9c01b5dd44e94d15b12a7cc44533e82ef9aa20d3
Sujan Maji <dev.sujanmaji@gmail.com>
2024-02-08 16:29:18 +0530

    add font-bold

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,6 @@
 const Header = ({ children }) => {
   return (
-    <header className="sticky left-0 right-0 top-0 z-10 bg-white py-2 text-center text-xl shadow-md">
+    <header className="sticky left-0 right-0 top-0 z-10 bg-white py-2 text-center text-xl font-bold shadow-md">
       {children}
     </header>
   );
